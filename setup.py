8915d0d6ff67fb03013f4038a89e6f56533ae162
Vivek Chand <vivekchand19@gmail.com>
2014-06-03 12:17:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = """
  """
 
 setup(name='plasticparser',
-      version='0.1.6',
+      version='0.1.7',
       description='An Elastic Search Query Parser',
       long_description=long_description,
       url='https://github.com/Aplopio/plasticparser',
