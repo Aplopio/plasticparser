78248eabd51ce7ceab1feab5fb061b38f8855492
Vedarth Kulkarni <vedarthk@vedarthz.in>
2016-06-13 16:58:35 +0530

    bump version => v0.3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = """
 
 setup(
     name='plasticparser',
-    version='0.3.1',
+    version='0.3.2',
     description='An Elastic Search Query Parser',
     long_description=long_description,
     url='https://github.com/Aplopio/plasticparser',
